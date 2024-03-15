02e0eb46275a6bb737d8a3e4b492ee730b3042a5
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-03-15 21:51:03 +0200

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,15 @@
-"""Module to add two floats"""
+"""Module to add two floats and return the result in float"""
 
 
 def add(a: float, b: float) -> float:
-    """Add two floats and return the result"""
+    """
+    Add two floats and return the result.
+
+    Args:
+        a (float): The first float number.
+        b (float): The second float number.
+
+    Returns:
+        float: The sum of the two input floats.
+    """
     return a + b
